92cc634d3c3e5b03b7d437a3d571a0096cb83fac
user <user@DESKTOP-Q96C748>
2024-11-11 17:10:08 +0300

    Добавьте файлы проекта.

---
+++ TodoApp/NewFolder2/TodoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.NewFolder2
+{
+    internal class TodoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;      // Изучить, что такое Классы, Поля, Методы и вернуться к написанию функционала
+        
+    }
+}
